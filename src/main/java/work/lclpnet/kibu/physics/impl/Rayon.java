8896cc051b23e757bb46b08d8f23e1e7e40ee150
LCLP <lukas@lclpnet.work>
2025-07-20 19:38:00 +0200

    fix native library loading on the server and properly configure logging

--- src/main/java/work/lclpnet/kibu/physics/impl/Rayon.java
+++ src/main/java/work/lclpnet/kibu/physics/impl/Rayon.java
@@ -1,16 +1,21 @@
 package work.lclpnet.kibu.physics.impl;
 
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.objects.PhysicsRigidBody;
 import org.slf4j.Logger;
 import work.lclpnet.kibu.physics.KibuPhysics;
 import work.lclpnet.kibu.physics.impl.bullet.natives.NativeLoader;
 import work.lclpnet.kibu.physics.impl.event.ServerEventHandler;
+
+import java.util.logging.Level;
 
 public class Rayon {
 
 	public static final Logger LOGGER = KibuPhysics.LOGGER;
 
 	public static void intialize() {
-		java.util.logging.LogManager.getLogManager().reset();
+		configureLoggers();
 
 		NativeLoader loader = new NativeLoader(LOGGER);
 
@@ -19,6 +24,15 @@ public class Rayon {
 			return;
 		}
 
+
 		ServerEventHandler.register();
+	}
+
+	private static void configureLoggers() {
+		java.util.logging.Logger.getLogger("jSnapLoader").setLevel(Level.WARNING);
+
+		PhysicsSpace.logger.setLevel(Level.SEVERE);
+		PhysicsCollisionObject.logger.setLevel(Level.SEVERE);
+		PhysicsRigidBody.logger2.setLevel(Level.SEVERE);
 	}
 }
--- src/main/java/work/lclpnet/kibu/physics/impl/bullet/natives/NativeLoader.java
+++ src/main/java/work/lclpnet/kibu/physics/impl/bullet/natives/NativeLoader.java
@@ -1,5 +1,6 @@
 package work.lclpnet.kibu.physics.impl.bullet.natives;
 
+import com.jme3.bullet.util.NativeLibrary;
 import electrostatic4j.snaploader.LibraryInfo;
 import electrostatic4j.snaploader.LoadingCriterion;
 import electrostatic4j.snaploader.NativeBinaryLoader;
@@ -9,6 +10,8 @@ import electrostatic4j.snaploader.platform.util.PlatformPredicate;
 import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class NativeLoader {
@@ -21,10 +24,19 @@ public class NativeLoader {
 
     public boolean load() {
         Path path = getGameDir().resolve("natives");
+
+        try {
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            logger.error("Failed to create natives directory", e);
+            return false;
+        }
+
         var dirPath = new DirectoryPath(path.toString());
 
         LibraryInfo info = new LibraryInfo(null, "bulletjme", dirPath);
         NativeBinaryLoader loader = new NativeBinaryLoader(info);
+        loader.setLoggingEnabled(true);
 
         NativeDynamicLibrary[] libraries = {
                 new NativeDynamicLibrary("native/linux/arm64", PlatformPredicate.LINUX_ARM_64),
@@ -43,11 +55,18 @@ public class NativeLoader {
             return false;
         }
 
+        // check if native library is linked properly
+        try {
+            NativeLibrary.isDebug();
+        } catch (UnsatisfiedLinkError e) {
+            return false;
+        }
+
         logger.info("libbulletjme native library successfully loaded");
         return true;
     }
 
     static Path getGameDir() {
-        return FabricLoader.getInstance().getGameDir();
+        return FabricLoader.getInstance().getGameDir().toAbsolutePath();
     }
 }
